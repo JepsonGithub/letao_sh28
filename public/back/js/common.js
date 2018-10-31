d3a980760adfa7cde4cbb47b0256f69ff0ad5b45
jepson <jepsonpp@qq.com>
2018-10-31 08:59:10 +0800

    进度条功能完成

---
+++ public/back/js/common.js
@@ -0,0 +1,39 @@
+// 调用进度条方法
+
+// 开启进度条
+//NProgress.start();
+//
+//setTimeout(function() {
+//  // 结束进度条
+//  NProgress.done();
+//}, 1000);
+
+
+
+// ajax 全局事件
+// 需求: (1) 在 ajax 开始发送的时候, 开启进度条,
+//       (2) 在 所有的 ajax 请求结束时, 结束进度条
+
+
+// .ajaxComplete() 这个事件将在每个 ajax 完成时调用  (不管成功还是失败, 都会调用)
+// .ajaxSuccess()  在每个 ajax 成功时调用
+// .ajaxError()    将在每个 ajax 失败时调用
+// .ajaxSend()     在每个 ajax 发送前调用
+
+// .ajaxStart()    在第一个 ajax 开始发送时调用
+// .ajaxStop()     在所有的 ajax 请求完成后调用
+
+
+$(document).ajaxStart(function() {
+  // 第一个 ajax 开始发送时调用, 开启进度条
+  NProgress.start();
+});
+
+
+$(document).ajaxStop(function() {
+  // 在所有的 ajax 请求完成时调用
+  // 模拟网络环境, 添加延迟
+  setTimeout(function() {
+    NProgress.done();
+  }, 500 );
+})
