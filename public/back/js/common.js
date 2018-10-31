56d049f8a3d687e7f162dc4d07ac8300faa1b3a0
jepson <jepsonpp@qq.com>
2018-10-31 11:00:52 +0800

    公共的侧边栏切换, 退出功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -36,4 +36,58 @@ $(document).ajaxStop(function() {
   setTimeout(function() {
     NProgress.done();
   }, 500 );
+});
+
+
+
+$(function() {
+
+  // 1. 二级导航的切换
+  $('.lt_aside .nav .category').click(function() {
+    $(this).next().stop().slideToggle();
+  });
+
+
+  // 2. 左侧菜单的切换
+  $('.lt_topbar .icon_menu').click(function() {
+    // 切换侧边栏, hidemenu 表示隐藏侧边栏的状态
+    $('.lt_aside').toggleClass("hidemenu");
+    $(".lt_main").toggleClass("hidemenu");
+    $(".lt_topbar").toggleClass("hidemenu");
+  })
+
+
+  // 3. 退出功能的实现
+  // 显示退出模态框
+  $('.lt_topbar .icon_logout').click(function() {
+    // 显示模态框
+    // 显示 modal("show");
+    // 隐藏 modal("hide");
+    $('#logoutModal').modal("show");
+  });
+
+  // 退出功能
+  // 退出登陆的方式:
+  // (1) 用户端(浏览器端), 用户自己清除浏览器缓存  (清空了 cookie),
+  //     本质上将会话标识 sessionId 也清除了
+  // (2) 前端通过发送ajax退出请求, 让后台销毁当前用户的登录状态
+
+  $('#logoutBtn').click(function() {
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      // 如果后台在响应头中, 设置了 响应类型为 json 格式, 就可以省略 dataType
+      dataType: "json",
+      success: function( info ) {
+        console.log( info );
+        if ( info.success ) {
+          // 退出成功, 跳转到登陆页
+          location.href = "login.html";
+        }
+      }
+    })
+  })
+
+
+
 })
