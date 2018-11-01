ced7ef1b046318abcb62ba26fd7307d8477709dc
jepson <jepsonpp@qq.com>
2018-11-01 10:10:40 +0800

    一级分类-添加功能完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -42,7 +42,66 @@ $(function() {
         })
       }
     })
-  }
+  };
+
+
+
+  // 2. 点击添加按钮, 显示添加模态框
+  $('#addBtn').click(function() {
+    $('#addModal').modal("show");
+  });
+
+
+
+  // 3. 表单校验
+  $('#form').bootstrapValidator({
+    // 配置图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',   // 校验成功
+      invalid: 'glyphicon glyphicon-remove',   // 校验失败
+      validating: 'glyphicon glyphicon-refresh'  // 校验中
+    },
+
+    // 配置需要校验的字段
+    fields: {
+      categoryName: {
+        // 校验规则
+        validators: {
+          notEmpty: {
+            message: "请输入一级分类"
+          }
+        }
+      }
+    }
+  });
+
+
+
+  // 4. 注册表单校验成功事件, 阻止默认的提交, 通过 ajax 提交
+  $('#form').on("success.form.bv", function( e ) {
+    // 阻止默认的提交
+    e.preventDefault();
+
+    $.ajax({
+      type: "post",
+      url: "/category/addTopCategory",
+      data: $('#form').serialize(),
+      dataType: "json",
+      success: function( info ) {
+        console.log( info );
+        // 关闭模态框
+        $('#addModal').modal("hide");
+        // 页面重新渲染第1页
+        currentPage = 1;
+        render();
+
+        // 调用 resetForm 进行重置
+        // resetForm(true) 传 true 表示内容和校验状态都重置
+        $('#form').data("bootstrapValidator").resetForm( true );
+      }
+    })
+
+  })
 
 
 });
